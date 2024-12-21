d593a01f329f9ecf7015566c797011b73abb6b07
JaivardhanSinghShaktawat <shaktawatjaivardhansingh.dev@gmail.com>
2024-12-21 21:11:11 +0530

    Introduction To Flowchart & Pseudocode

---
+++ 2-Flowcharts/1-Flowcharts.cpp
@@ -0,0 +1,58 @@
+/*
+--> How to approach a problem ?
+   
+    1. read & understand the problem
+    2. read the inputs and constraints.
+    3. create a rough solution and dry run it.
+    4. code it
+    5. find errors and debug it
+    6. optimize the solution by using other solutions.
+
+--> What is a Flowchart ? 
+    A flowchart is the diagrametic representation of any solution or algorithm.
+    We use different diagrams to represent the approach or solution.
+
+--> Components Of Flowchart
+
+    1. Terminal/Terminator : It is oval in shape.It is used to start and to end the
+    program.
+
+    2. Parallelogram : It is used for input and output.To take input and to output
+    the result we use it.It is called as input output block.
+
+    3. Rectangle : It is used for processing like i = i + 1 ; 
+    Here we do calculation , initialization , declaration , processing , updation.
+    It is called as process block.
+
+    4. Diamond : It is called decision making block.It is used when we have to make
+    some decision based on some condition.This block always contains some 
+    condition.
+
+    5. Arrows : It is used to connect the different component of the flowchart and
+    it also shows the flow of the flowchart/program.
+    The arrows show the flow of execution.
+
+    6. Connector : It is used with functions.We can connect different flowcharts using
+    the connector.
+
+--> Problems solving with Flowcharts
+
+    1. Sum of two numbers
+    2. Average of two numbers
+    3. Find cube of a number
+    4. Find square of a number
+    5. Number is even or odd
+    6. Number is positive or negative
+    7. Print a number 10 times
+    8. Print n natural numbers
+    9. Sum of n natural numbers
+    10. Check if a number is prime or not.
+  
+--> What is pseudocode ? 
+    It is the way to represent any solution which is generic.
+    We explain solution by using english words only.
+
+    We use flowchart and pseudocode to explain our solution to other people and can dry 
+    run the code.
+
+*/
